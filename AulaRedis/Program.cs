f49a18aa90c674152e7a308e56545af25c144c1d
paulogentile <paulo_gentile@msn.com>
2019-02-07 21:10:32 -0200

    Add project files.

---
+++ AulaRedis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AulaRedis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
