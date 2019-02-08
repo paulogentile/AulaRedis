347d8ed36cee572d4ccf3be938a2695a5ea64639
paulogentile <paulo_gentile@msn.com>
2019-02-07 22:37:13 -0200

    Inicial

--- AulaRedis/Program.cs
+++ AulaRedis/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StackExchange.Redis;
+using System;
 
 namespace AulaRedis
 {
@@ -6,7 +7,38 @@ namespace AulaRedis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var redis = ConnectionMultiplexer.Connect("localhost");
+            var db = redis.GetDatabase();
+
+            //Console.WriteLine("SET A 1");
+            //db.StringSet("A", "1");
+
+            //Console.WriteLine($"GET A: {db.StringGet("A")}");
+
+            //Console.WriteLine("INCR A");
+            //db.StringIncrement("A");
+
+            //Console.WriteLine($"GET A: {db.StringGet("A")}");
+
+            //Console.WriteLine("SADD tech SQL");
+            //db.SetAdd("tech", "SQL");
+
+            //Console.WriteLine("HSET CLI AA 1");
+            //db.HashSet("CLI", new HashEntry[] { new HashEntry("AA", "1") });
+
+            //Console.WriteLine("LPUSH L1 A");
+            //db.ListRightPush("L1", "A");
+
+            //Console.WriteLine("LPUSH L1 B");
+            //db.ListRightPush("L1", "B");
+
+            var sub = redis.GetSubscriber();
+            sub.Subscribe("14NET", (ch, msg) =>
+            {
+                Console.WriteLine(msg.ToString());
+            });
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Oraculo/Program.cs
@@ -0,0 +1,56 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Oraculo
+{
+    class Program
+    {
+        private const string RedisConnectionString = "localhost";
+        //private const string RedisConnectionString = "40.77.30.246";
+        private static ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(RedisConnectionString);
+        private static IDatabase db = connection.GetDatabase();
+
+        private const string ChatChannel = "Perguntas";
+        private static readonly string userName = "HanSolo";
+
+        static void Main(string[] args)
+        {
+            // Create pub/sub
+            var pubsub = connection.GetSubscriber();
+
+            // Subscriber subscribes to a channel
+            pubsub.Subscribe(ChatChannel, (channel, message) => MessageAction(message));
+
+            Console.WriteLine("Conectado ao Cana Perguntas, aguardando mensagens.");
+
+            // Messaging here
+            while (true)
+            {
+                var resposta = Console.ReadLine();
+                db.HashSet(pergunta, new HashEntry[] { new HashEntry(userName, resposta) });
+                //pubsub.Publish(ChatChannel, $"{userName}: {Console.ReadLine()}");
+            }
+        }
+
+        private static string pergunta = "";
+
+        static void MessageAction(string message)
+        {
+            int initialCursorTop = Console.CursorTop;
+            int initialCursorLeft = Console.CursorLeft;
+
+            Console.MoveBufferArea(0, initialCursorTop, Console.WindowWidth, 1, 0, initialCursorTop + 1);
+            Console.CursorTop = initialCursorTop;
+            Console.CursorLeft = 0;
+
+            // Separa a pergunta do número
+            var arrPergunta = message.Split(' ');
+
+            pergunta = arrPergunta[0].Replace(":", "");
+            Console.WriteLine(message);
+
+            Console.CursorTop = initialCursorTop + 1;
+            Console.CursorLeft = initialCursorLeft;
+        }
+    }
+}
